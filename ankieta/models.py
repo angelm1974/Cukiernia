d4e3204b4804692264bb8156d050a29bb16d03a5
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-03-13 11:19:19 +0100

    panel administracyjny

--- ankieta/models.py
+++ ankieta/models.py
@@ -1,10 +1,19 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class Pytanie(models.Model):
     tekst_pytania=models.CharField(max_length=300)
     data_publikacji=models.DateTimeField()
+    
+    def byl_publikowany_ostatnio(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.data_publikacji <= now
+    byl_publikowany_ostatnio.admin_order_field ='data_publikacji'
+    byl_publikowany_ostatnio.boolean =True
+    byl_publikowany_ostatnio.short_description ="Publikowany ostatnio?"
     
     def __str__(self):
         return self.tekst_pytania
--- ankieta/views.py
+++ ankieta/views.py
@@ -1,29 +1,35 @@
 from django.http import Http404
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-
+from django.views import generic
 
 from .models import Pytanie, Wybor
 # Create your views here.
 
-def index(request):
+class IndexView(generic.ListView):
+    template_name='ankieta/index.html'
+    context_object_name= 'lista_pytan'
+    queryset = Pytanie.objects.filter(data_publikacji__lte=timezone.now()).order_by('-data_publikacji')[:5]
 
-    lista_pytan = Pytanie.objects.order_by('-data_publikacji')[:5]
-    contex= {'lista_pytan' : lista_pytan,}
-    return render(request,'ankieta/index.html',contex)
+class DetaleView(generic.DetailView):
+    model = Pytanie
+    template_name = 'ankieta/detale.html'
 
-def detale(request, pytanie_id):
+class WynikiView(generic.DetailView):
+    model = Pytanie
+    template_name = 'ankieta/wyniki.html'
+    
+""" def detale(request, pytanie_id):
     try:
         pytanie = Pytanie.objects.get(pk = pytanie_id)
     except Pytanie.DoesNotExist:
         raise Http404('Nie ma takiego pytania!!!')
-    return render(request,'ankieta/detale.html',{'pytanie':pytanie})
+    return render(request,'ankieta/detale.html',{'pytanie':pytanie}) """
 
-def wyniki(request, pytanie_id):
-    pytanie = get_object_or_404(Pytanie,pk=pytanie_id)
-    return render(request,'ankieta/wyniki.html',{'pytanie':pytanie})
+
 
 def glosuj(request, pytanie_id):
     pytanie = get_object_or_404(Pytanie,pk=pytanie_id)
@@ -36,6 +42,6 @@ def glosuj(request, pytanie_id):
     else:
         wybrany.glosy +=1
         wybrany.save()
-        return HttpResponseRedirect(reverse('wyniki',args=(pytanie.id,)))
+        return HttpResponseRedirect(reverse('ankieta:wyniki',args=(pytanie.id,)))
     
     
